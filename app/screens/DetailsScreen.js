5f30ac9903672c6d8e7695b9011637b7580d3b69
susanka068 <sathinandimajumder068@gmail.com>
2020-08-14 10:45:12 +0530

    details page updated

--- app/screens/DetailsScreen.js
+++ app/screens/DetailsScreen.js
@@ -2,11 +2,25 @@ import * as React from "react";
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Container, Header, Content, List, ListItem } from "native-base";
 
 export default function DetailsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details</Text>
-    </View>
+    <Container>
+      <Header />
+      <Content>
+        <List>
+          <ListItem>
+            <Text>me</Text>
+          </ListItem>
+          <ListItem>
+            <Text>main</Text>
+          </ListItem>
+          <ListItem>
+            <Text>my</Text>
+          </ListItem>
+        </List>
+      </Content>
+    </Container>
   );
 }
